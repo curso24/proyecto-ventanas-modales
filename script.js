214fad0f4a1231586ceb26f242372737a9bfe65e
curso24 <tripleene333@gmail.com>
2024-01-18 14:23:45 +0100

    creamos las funciones,

---
+++ script.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+element.classList.add(nombreClase);
+element.classList.remove(nombreClase);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+	
+
