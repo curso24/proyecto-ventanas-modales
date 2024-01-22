41b131cc10d3a7bda0d2b8677cc4b06d088fb98c
curso24 <tripleene333@gmail.com>
2024-01-22 12:59:39 +0100

    REFACTORIZO

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-element.classList.add(nombreClase);
-element.classList.remove(nombreClase);
+element.classList.add(nombreClase)
+element.classList.remove(nombreClase)
 
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-	
-
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
