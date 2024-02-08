05c9a0130ee4e81d3b505a6e393c5be5132ec427
curso24 <tripleene333@gmail.com>
2024-02-08 12:35:22 +0100

    deploy y  levantar docker

--- README.md
+++ README.md
@@ -7,9 +7,13 @@
 - La documentación la tenemos en Obsidian en el apartado 4.Fundamentos del DOM
 -Añadir script llamado deploy para el envio del programa al servidor en la nube (se asocia al rsync)
 
+  
+
 -Añadir script llamado build para que haga un minify del css, html y javascript
 
     Investigar paquetes que podemos instalar de npm
     Instalar paquetes con npm install
     Configurar paquetes si necesario (leer doc de cada paquete)
 
+
+ https://barbara.certweb.infenlaces.com/ventanas-modales/
--- script.js
+++ script.js
@@ -1,17 +1,50 @@
+//Selección de elementos del DOM:
+
 const modal = document.querySelector('.modal')
+
 const overlay = document.querySelector('.overlay')
+
 const btnCloseModal = document.querySelector('.close-modal')
+
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+/*
+
 element.classList.add(nombreClase)
+
 element.classList.remove(nombreClase)
+
+*/
+
+// Funciones de apertura y cierre del modal:
 
 const openModal = function () {
   modal.classList.remove('hidden')
+
   overlay.classList.remove('hidden')
 }
 
 const closeModal = function () {
   modal.classList.add('hidden')
+
   overlay.classList.add('hidden')
 }
+
+//Event Listeners: Se agregan event listeners a los botones para abrir el modal
+
+// iteración sobre botones para abrir modal y control de cierre con teclado
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal)
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e, e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
